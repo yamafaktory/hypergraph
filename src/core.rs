81bc56da16518e4ca53e31f0f395e91bb0af6f84
Davy Duperron <yamafaktory@gmail.com>
2020-11-15 20:59:27 +0100

    chore: implement Dijkstra algorithm (#3)

--- src/core.rs
+++ src/core.rs
@@ -4,6 +4,8 @@ pub(super) use crate::private::ExtendedDebug;
 use indexmap::{IndexMap, IndexSet};
 use itertools::Itertools;
 use std::{
+    cmp::Ordering,
+    collections::BinaryHeap,
     fmt::{Debug, Formatter, Result},
     hash::Hash,
 };
@@ -241,6 +243,100 @@ where
     /// Gets the list of all vertices connected to a given vertex.
     pub fn get_vertex_connections(&self, from: VertexIndex) -> Vec<VertexIndex> {
         self.get_connections(from, None)
+    }
+
+    /// Gets a list of the shortest path of vertices between two vertices.
+    /// The implementation of the algorithm is based on
+    /// https://doc.rust-lang.org/std/collections/binary_heap/#examples
+    pub fn get_dijkstra_connections(
+        &self,
+        from: VertexIndex,
+        to: VertexIndex,
+    ) -> Option<Vec<VertexIndex>> {
+        #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+        struct Cursor {
+            distance: usize,
+            index: usize,
+        }
+
+        // Use a custom implementation of Ord as we want a min-heap BinaryHeap.
+        impl Ord for Cursor {
+            fn cmp(&self, other: &Cursor) -> Ordering {
+                other
+                    .distance
+                    .cmp(&self.distance)
+                    .then_with(|| self.distance.cmp(&other.distance))
+            }
+        }
+
+        impl PartialOrd for Cursor {
+            fn partial_cmp(&self, other: &Cursor) -> Option<Ordering> {
+                Some(self.cmp(other))
+            }
+        }
+
+        // We need to initialize a vector of length equal to the number of vertices.
+        // The default value, as per Dijkstra, must be set to infinity.
+        // A value of usize::MAX is used.
+        let mut distances = (0..self.vertices.len())
+            .map(|_| usize::MAX)
+            .collect::<Vec<usize>>();
+
+        // Create an empty binary heap.
+        let mut heap = BinaryHeap::new();
+
+        // Initialize the first vertex to zero.
+        distances[from] = 0;
+
+        // Push the first cursor to the heap.
+        heap.push(Cursor {
+            distance: 0,
+            index: from,
+        });
+
+        // Keep track of the traversal path.
+        let mut path = Vec::<usize>::new();
+
+        while let Some(Cursor { distance, index }) = heap.pop() {
+            // End of the traversal.
+            if index == to {
+                // We need to inject the index of the target vertex.
+                path.push(to);
+
+                // Remove duplicates generated during the iteration of the algorithm.
+                path.dedup();
+
+                return Some(path);
+            }
+
+            // Skip if a better path has already been found.
+            if distance > distances[index] {
+                continue;
+            }
+
+            // For every connected vertex, try to find the lowest distance.
+            for vertex_index in self.get_vertex_connections(index) {
+                let next = Cursor {
+                    // We assume a distance of one by default.
+                    distance: distance + 1,
+                    index: vertex_index,
+                };
+
+                // If so, add it to the frontier and continue.
+                if next.distance < distances[next.index] {
+                    // Update the traversal accordingly.
+                    path.push(index);
+
+                    // Push it to the heap.
+                    heap.push(next);
+
+                    // Relaxation, we have now found a better way
+                    distances[vertex_index] = next.distance;
+                }
+            }
+        }
+
+        None
     }
 
     /// Renders the hypergraph to Graphviz dot format.
